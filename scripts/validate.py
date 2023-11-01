69c67ac98a45b1360cf90676d579632bc9a50916
AnthonyFuller <24512050+AnthonyFuller@users.noreply.github.com>
2023-11-01 02:11:06 +0000

    feat: add validate script

---
+++ scripts/validate.py
@@ -0,0 +1,62 @@
+'''
+Validates various things that need to be validated, these include:
+- LINE collisions [1]
+- Strings matching hashes [2]
+'''
+
+from crc import Calculator, Crc32
+import glob, json, sys
+
+calculator = Calculator(Crc32.CRC32)
+
+# Returns number of LINE collisions
+def checkCollisions():
+    hashes = []
+    for path in glob.glob("../extracted/H1/**/*.LOCR.JSON", recursive=True):
+        j = json.load(open(path, "r", encoding="utf-8"))
+        for hash in j["languages"]["xx"].keys():
+            if hash in hashes:
+                print(f"Collision found! \"{path}\" - {hash}")
+            else:
+                hashes.append(hash)
+
+
+def checkHashes():
+    SOUNDTAGS = json.load(open("../soundtags.json", "r"))
+    CASES = json.load(open("../cases.json", "r"))
+    LINES = json.load(open("../lines.json", "r"))
+
+    for hash, value in SOUNDTAGS.items():
+        if value == None:
+            continue
+
+        if hash != f"{calculator.checksum(str.encode(value)):08X}":
+            print(f"Soundtag mismatch: \"{hash}\" - \"{value}\"")
+
+    for hash, value in CASES.items():
+        if value == None:
+            continue
+
+        if hash != f"{calculator.checksum(str.encode(value)):08X}":
+            print(f"Case mismatch: \"{hash}\" - \"{value}\"")
+
+    for hash, value in LINES.items():
+        if value == None:
+            continue
+
+        if hash != f"{calculator.checksum(str.encode(value)):08X}":
+            print(f"LINE mismatch: \"{hash}\" - \"{value}\"")
+
+if len(sys.argv) != 2:
+    print("Invalid arguments!")
+    print("Usage: py validate.py <mode>")
+    print("Valid modes: 1 and 2")
+    exit(1)
+
+if sys.argv[1] == "1":
+    checkCollisions()
+elif sys.argv[1] == "2":
+    checkHashes()
+else:
+    print("Invalid mode!")
+    print("Valid modes: 1 and 2")
